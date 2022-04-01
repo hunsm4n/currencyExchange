e9fbc2e405e2fa70ab8afb071abce8a4598b045c
ByeongHun <you@example.com>
2022-04-02 02:22:04 +0900

    second commit

--- main.js
+++ main.js
@@ -1,17 +1,51 @@
-// 1. 박스 두개 만들기
+const currencyRatio = {
+    USD: {
+        KRW: 1184.36,
+        USD: 1,
+        VND: 22839.03,
+        unit: "달러"
+    },
+    KRW: {
+        KRW: 1,
+        USD: 0.00082,
+        VND: 18.73,
+        unit: "원"
+    },
+    VND: {
+        KRW: 0.053,
+        USD: 0.000044,
+        VND: 1,
+        unit: "동"
+    }
+}
 
-// 2. 드랍다운 리스트 만들기
+// 값을 재할당 하기 위해서 let으로 선언
+let fromCurrency = 'USD';
 
-// 3. 환율정보들고오기
 
-// 4. 드랍다운 리스트에서 아이템 선택하면 아이템 바뀜
+// console.log(currencyRatio.VND.unit);
 
-// 5. 금액을 입력하면 환전이 된다.
+// console.log(currencyRatio["VND"]["unit"])
 
-// 6. 드랍다운 리스트에서 아이템을 선택하면 그 단위 기준으로 환전이 됨
+document.querySelectorAll("#from-currency-list a").forEach((menu) => menu.addEventListener("click", function (item) {
+    item.addEventListener("click", function () {
+        toCurrency = this.id;
+        toButton.innerHTML = `<img class="flag-img"src="${currencyRatio[toCurrency].img}"/>${toCurrency}`;
+        convert("from");
+    })
 
-// 7. 숫자를 한국어로 읽는 법
+})
+);
 
-// 8. 5000
+document.querySelectorAll("#to-currency-list a").forEach((menu) => menu.addEventListener("click", function () {
+    document.getElementById("to-button").textContent = this.textContent;
+})
+);
 
-// 개선사항 -> 암호화폐 와 원화 , 리액트로 리팩토링
+// 1. 키를 입력하는 순간 
+// 2. 환전이 되어서 
+// 3. 환전된 값이 보인다
+
+function convert() {
+    console.log("here");
+}
